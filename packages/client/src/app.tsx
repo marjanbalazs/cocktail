74c283116ad4ff521e6e816c3ed82b6dd5d1e9b0
Balazs <marjanbalazs@outlook.com>
2022-04-16 18:55:02 +0200

    Add frotnend app start

---
+++ packages/client/src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+const App = () => {
+    return (
+        <div>Cocktail App</div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
